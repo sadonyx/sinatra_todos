ad94e46dee6a8d19e7fb19113c27c0906b699f4e
Adnan Shihabi <adnan@ashihabi.me>
2023-09-09 23:46:03 -0700

    Added identifiers to Todo Items 	- Resolved bug introduced after adding JS code in which using the index of a todo item as a todo ID was not sufficient when deleting any todo item. 		- Resolved by including unique ID values for each todo item within a list

--- todo.rb
+++ todo.rb
@@ -9,6 +9,11 @@ configure do
   set :erb, :escape_html => true
 end
 
+def next_todo_id(todos)
+  max = todos.map { |todo| todo[:id] }.max || 0
+  max + 1
+end
+
 # Return an error message if the name is invalid. Return nil if name is valid.
 def error_for_list_name(name)
   if !(1..100).cover? name.size
@@ -28,6 +33,14 @@ def find_list_by_id(id)
 
   session[:error] = "The specified list was not found."
   redirect "/lists"
+end
+
+def find_todo_by_id(list, todo_id)
+  todo = list[:todos].find { |todo| todo.key(todo_id) }
+  return todo if todo
+
+  session[:error] = "The specified todo was not found."
+  redirect "/lists/#{id}"
 end
 
 def error_for_todo(list, text)
@@ -132,17 +145,18 @@ post "/lists/:id/todos" do
     session[:error] = error
     erb :list, layout: :layout
   else
-    @list[:todos] << { name: params[:todo], completed: false } # Append todo object to todo array of list object
+    todo_id = next_todo_id(@list[:todos])
+    @list[:todos] << { id: todo_id, name: params[:todo], completed: false } # Append todo object to todo array of list object
     session[:success] = "The todo '#{params[:todo]}' was added successfully."
     redirect "/lists/#{params[:id]}"
   end
 end
 
 # Delete todo item
-post "/lists/:id/todos/:index/destroy" do
+post "/lists/:id/todos/:todo_id/destroy" do
   @list = find_list_by_id(params[:id])
-  index = params[:index].to_i
-  deleted_item = @list[:todos].delete_at(index)[:name]
+  todo = find_todo_by_id(@list, params[:todo_id])
+  deleted_item = @list[:todos].delete(todo)[:name]
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     status 204
   else
@@ -165,13 +179,13 @@ post "/lists/:id/complete_all" do
 end
 
 # Toggle completion status of todo
-post "/lists/:id/todos/:index" do
+post "/lists/:id/todos/:todo_id" do
   @list = find_list_by_id(params[:id])
-  index = params[:index].to_i
-  name = @list[:todos][index][:name]
+  todo = find_todo_by_id(@list, params[:todo_id])
+  name = todo[:name]
   is_completed = params[:completed] == 'true'
 
-  @list[:todos][index][:completed] = is_completed
+  todo[:completed] = is_completed
   @list[:all_completed] = check_completion_of_all_todos(@list)
   session[:success] = "The todo '#{name}' has been marked as #{is_completed ? 'completed' : 'incomplete'}. #{ @list[:all_completed] ? "'#{@list[:name]}' is complete!" : '' }"
   redirect "/lists/#{params[:id]}"
@@ -193,5 +207,12 @@ helpers do
   # Returns total size of todo list
   def todos_count(list)
     list[:todos].size
+  end
+
+  def sort_todos(todos, &block)
+    complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
+
+    incomplete_todos.each(&block)
+    complete_todos.each(&block)
   end
 end
